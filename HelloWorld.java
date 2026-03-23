0180d9d854e18d7f87d12f052d1c54d5bd43cc59
ugowtham211-ctrl <ugowtham211@gmail.com>
2026-03-23 10:48:39 +0530

    uc5

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,38 +1,16 @@
-/**
- * OOPSBannerApp UC4 - Render OOPS as Banner using String Array and Loop
- *
- * This use case improves upon UC3 by using a String array to store banner lines
- * and iterating through them with a for-each loop, eliminating hardcoded print
- * statements and improving modularity and reusability.
- *
- * @author Developer
- * @version 4
- */
+public class OOPS {
+    public static void main(String[] args) {
 
-// Extend the user story 3 to further develop the OOPS Banner Application
-// by using String arrays to hold banner lines and printing them in a loop.
-// This use case improves upon UC3 by using a String array to store banner lines
-// and iterating through them with a for-each loop, eliminating hardcoded print
-// statements and improving modularity and reusability.
-public class OOPSBannerArray {
-     
+        String[] oops = {
+            " ***    ***    ****    ****  ",
+            "*   *  *   *   *   *  *      ",
+            "*   *  *   *   ****    ****  ",
+            "*   *  *   *   *           *  ",
+            " ***    ***    *       ****    "
+        };
 
-    public static void main(String[]  args){
-
-          String[] lines = new String[7];
-
-
-        lines[0] = String.join(" ", " *** ", " *** ", " **** ", " **** ");
-        lines[1] = String.join(" ", "*   *", "*   *", "*    *", "*     ");
-        lines[2] = String.join(" ", "*   *", "*   *", "*    *", "*     ");
-        lines[3] = String.join(" ", "*   *", "*   *", "****  ", " ***  ");
-        lines[4] = String.join(" ", "*   *", "*   *", "*     ", "    * ");
-        lines[5] = String.join(" ", "*   *", "*   *", "*     ", "*   * ");
-        lines[6] = String.join(" ", " *** ", " *** ", "*     ", " ***  ");
-
-        // Use for-each loop to print each line
-        for (String line : lines) {
+        for (String line : oops) {
             System.out.println(line);
-                                 }
-                                            }
-                                }
+        }
+    }
+}
