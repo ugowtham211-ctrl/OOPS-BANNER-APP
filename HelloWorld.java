46f7194a02b8ac3921539438c8d50fea1dd5b79f
ugowtham211-ctrl <ugowtham211@gmail.com>
2026-03-23 10:37:47 +0530

    uc2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
-public class HelloWorld{
- public static void main(String args[]){
-     System.out.println("OOPS");
-    }   
+public class OOPSBannerApp{
+    // Main method to run the banner display
+    // Print each line of the OOPS banner to create the visual effect for the // message "OOPS". Construct each line using string concatenation.
+    // Each line corresponds to a row in the banner for the letters 0, 0, P, S.
+    // Using individual print statements for each line.
+    public static void main (String[] args){
+        System.out.println("   ***   " + "    ***   " + " ******   " + "   ***** ");
+        System.out.println(" **   ** " + "  **   ** " + " **    ** " + " **      ");
+        System.out.println("**     **" + " **     **" + " **    ** " + "**       ");
+        System.out.println("**     **" + " **     **" + " ******   " + "  *****  ");
+        System.out.println("**     **" + " **     **" + " **       " + "      ** ");
+        System.out.println(" **   ** " + "  **   ** " + " **       " + " **   ** ");
+        System.out.println("   ***   " + "    ***   " + " **       " + "  *****  ");
+    }
 }
