f2dacfd866cbcbeced8d297bf837e6ef9c858f8c
ugowtham211-ctrl <ugowtham211@gmail.com>
2026-03-23 10:51:30 +0530

    uc6

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,55 @@
-public class OOPS {
-    public static void main(String[] args) {
-
-        String[] oops = {
-            " ***    ***    ****    ****  ",
-            "*   *  *   *   *   *  *      ",
-            "*   *  *   *   ****    ****  ",
-            "*   *  *   *   *           *  ",
-            " ***    ***    *       ****    "
-        };
-
-        for (String line : oops) {
-            System.out.println(line);
-        }
+public class OOPSBannerappp {
+      public static String[] getOPattern() {
+     return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+           };
     }
+
+    
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    
+      public static String[] getSPattern() {
+        return new String[]{
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+      }
+
+      public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + "  " +
+                oPattern[i] + "  " +
+                pPattern[i] + "  " +
+                sPattern[i]
+             );
+     }
+       }
 }
